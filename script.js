ce3c6e65e230c2ad25b2ec468e562ed31d4dcd7e
AkshayBenny <akshaybennygh@gmail.com>
2021-08-20 20:13:26 +0530

    js update

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-function add() {
+
+
+
+
+
+function add(a,b) {
+    return a + b ;
 }
+
+function sub(a,b) {
+    return a - b ;
+}
+
+function mul(a,b) {
+    return a * b ;
+}
+
+function div(a,b) {
+    return a / b ;
+}
+
+function mod(a,b) {
+    return a % b ;
+}
+
