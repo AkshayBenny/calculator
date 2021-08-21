a912ca5d2be80b7423da48102de11a99d4c2ba6e
AkshayBenny <akshaybennygh@gmail.com>
2021-08-21 13:55:03 +0530

    update

--- script.js
+++ script.js
@@ -22,11 +22,11 @@ function sub(a,b) {
     return a - b ;
 }
 
-function mul(a,b) {
+function multiply(a,b) {
     return a * b ;
 }
 
-function div(a,b) {
+function divide(a,b) {
     return a / b ;
 }
 
