1f5d4a81f17bc82d794cb258b01bc272d905c5d4
AkshayBenny <akshaybennygh@gmail.com>
2021-08-21 14:06:45 +0530

    update

--- script.js
+++ script.js
@@ -13,26 +13,27 @@ for (var i = 0; i < 11; i++) {
 
 
 
-
-function add(a,b) {
-    return a + b ;
-}
-
-function sub(a,b) {
-    return a - b ;
-}
-
-function multiply(a,b) {
-    return a * b ;
-}
-
-function divide(a,b) {
-    return a / b ;
-}
-
-function mod(a,b) {
-    return a % b ;
-}
+if (document.querySelector(".btn").value() === "add" ) {
+    function add(a,b) {
+        return a + b ;
+    }
+} else if (document.querySelector(".btn").value() === "subtract" ){
+    function sub(a,b) {
+        return a - b ;
+    }
+} else if (document.querySelector(".btn").value() === "multiply" ){
+    function multiply(a,b) {
+        return a * b ;
+    }
+} else if (document.querySelector(".btn").value() === "divide" ){
+    function divide(a,b) {
+        return a / b ;
+    }
+} else if (document.querySelector(".btn").value() === "mod" ){
+    function mod(a,b) {
+        return a % b ;
+    }    
+} 
 
 
 
