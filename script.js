98fa30e426fa296af4ce8f10868d4e6ecc01d131
AkshayBenny <akshaybennygh@gmail.com>
2021-08-21 10:58:43 +0530

    function update

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+var elementIsClicked = false; 
+
+function clickHandler(){ 
+  elementIsClicked = true;
+}
+
+
+for (var i = 0; i < 11; i++) {
+    var element = document.querySelectorAll('.number')[i]; 
+    element.addEventListener('click', clickHandler); 
+}
 
 
 
@@ -23,3 +34,8 @@ function mod(a,b) {
     return a % b ;
 }
 
+
+
+function output(a,b,operator) {
+    return operator(a,b);
+}
