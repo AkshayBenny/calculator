96e90d87517bff477609c4f52c517a12155ce67f
AkshayBenny <akshaybennygh@gmail.com>
2021-08-23 15:42:47 +0530

    del

--- script.js
+++ script.js
@@ -11,36 +11,6 @@ for (var i = 0; i < 11; i++) {
 }
 
 
-if (document.querySelector(".btn").value() === "add" ) {
-    function add(a,b) {
-        op = a + b;
-        return operator = add ;
-    }
-} else if (document.querySelector(".btn").value() === "subtract" ){
-    function sub(a,b) {
-        op = a - b ;
-        return operator = sub ;
-    }
-} else if (document.querySelector(".btn").value() === "multiply" ){
-    function multiply(a,b) {
-        op = a * b ;
-        return operator = multiply
-    }
-} else if (document.querySelector(".btn").value() === "divide" ){
-    function divide(a,b) {
-        op = a / b ;
-        return operator = divide;
-    }
-} else if (document.querySelector(".btn").value() === "mod" ){
-    function mod(a,b) {
-        op = a % b ;
-        return operator = mod;
-    }    
-} else {
-    return null;
-}
-
-
 
 function output(a,b,operator) {
     operator(a,b);
