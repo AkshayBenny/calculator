cc3dce35c5ec399ddb34d46d9d5a760b7593e262
AkshayBenny <akshaybennygh@gmail.com>
2021-08-23 16:53:09 +0530

    update

--- script.js
+++ script.js
@@ -1,20 +1,54 @@
-var elementIsClicked = false; 
+var operator, calutated_value,a = 0, b = 0;
 
-function clickHandler(){ 
-  elementIsClicked = true;
+
+
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+function mod(a,b) {
+    return a%b;
 }
 
 
-for (var i = 0; i < 11; i++) {
-    var element = document.querySelectorAll('.number')[i]; 
-    element.addEventListener('click', clickHandler); 
-}
 
+function operator_finder() {
+    for (var i = 0 ; i <=5 ; i++) {
+        var btn_value = document.querySelectorAll(".sign")[i].value;
+        if (btn_value == "add"){
+            return operator = add;
+        } else if (btn_value == "subtract"){
+            return operator = subtract;
+        } else if (btn_value == "multiply"){
+            return operator = multiply;
+        } else if (btn_value == "divide"){
+            return operator = divide;
+        } else if (btn_value == "mod"){
+            return operator = mod;
+        } else {
+            return null;
+        }
+
+    }
+    
+    
+}
 
 
 function output(a,b,operator) {
-    operator(a,b);
-    return op;
+    return calculated_value = operator(a,b);
 }
 
-document.querySelector(".current-operand p").innerHTML = "op";
+document.querySelector(".current-operand p").innerHTML = "calculated_value";
